4d7aecf0644f30cc49b1abb7fc812922650f0f57
frand-lebaza <frandlebaza@gmail.com>
2025-10-01 10:02:08 -0500

    static files render

--- backend/build.sh
+++ backend/build.sh
@@ -4,5 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Crear directorio staticfiles si no existe
+mkdir -p staticfiles
+
 python manage.py collectstatic --no-input
 python manage.py migrate
